926dc72f896757b1a11441e7b81ef14c4bf1b01f
Jean-baptiste JAILLET <bajai@payback.dhcp.idf.intranet>
2014-09-23 18:20:38 +0200

    add functions checkcolumn (still need to debug there is bug in the algorithm) and grilleEmpty. Also add decideColumn, but just the first partial skeleton (this will be the main function of the program). It permits to check every function i'll need to decide where to play. Going step by step

--- bestaplayer.py
+++ bestaplayer.py
@@ -52,9 +52,58 @@ class BestaPlayer:
         """
         with open(self.fichier, 'r') as fi:
             for line in fi.readlines():
-                for car in line:
+                for car in line[:len(line) - 1]:
                     if car != '0':
                         return 2
             return 1
+
+    def grilleEmpty(self):
+        """
+        Implement function to check if the grid is empty.
+
+        """
+        for line in self.grille:
+            for car in line[:len(line) - 1]:
+                if car != '0':
+                    return False
+        return True
+
+    def checkColumn(self, player, inARow):
+        """
+        Implements function to check the current columns setup to evaluate best combinaison.
+
+        :param player: check for your numbers (your player number) or those of your opponent.
+        :param inARow: how many tokens in a row (3 or 2).
+        :return: true or false
+
+        """
+        count = 0
+        flag = False
+        for line in self.grille:
+            count = 0
+            for car in line[:len(line) - 1]:
+                if car == player and not flag:
+                    count = 1
+                elif car == player and flag:
+                    count += 1
+                    if count == inARow:
+                        return True
+                else:
+                    count = 0
+        return False
+
+    def decideColumn(self):
+        """
+        Implements main function : to decide what is the better hit to do.
+
+        :return: an int, representing the column where we play
+        """
+
+        if self.grilleEmpty():
+            return 3
+
+
+
 test = BestaPlayer('grille.txt')
 test.displayGrid()
+print test.checkColumn(test.player, 3)
